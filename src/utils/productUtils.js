dfb2ada1a21101340d9fd5184c87e9633d25fb10
Clarena Rodriguez <clarena.rodriguez@mvm.com.co>
2022-02-14 15:24:28 -0500

    feat: Set filter categories

--- src/utils/productUtils.js
+++ src/utils/productUtils.js
@@ -1,17 +1,40 @@
 const config = require("../../config");
 
 const handleSearchResponse = (data) => {
+    let categoryName = "";
+    const categories = [];
     const productList = data.results.map(
         (product) => mapProductItem(product)
     );
+    const productCategoryList = data.results.map((w) => w.category_id);
+    const categoriesList = data.filters.find((w) => w.id === 'category').values;
 
-    const productCategorieList = data.filters.find((w) => w.id === 'category');
-    const categories = productCategorieList === undefined ? []:
-        productCategorieList.values.map(w => w.name);
+    if (productCategoryList == undefined) { categories = []; }
+    else {
+        productCategoryList.forEach(category => {
+            categoryName = categoriesList.find(p => p.id == category).name;
 
+            if (categories.length > 0) {
+                indxCategoria = categories.findIndex(p => p.id == category);
+                if (indxCategoria > -1) {
+                    categories[indxCategoria] = {
+                        id: category,
+                        title: categoryName,
+                        count: (categories[indxCategoria].count + 1)
+                    }
+                    return;
+                }
+            } 
+            categories.push({
+                id: category,
+                title: categoryName,
+                count: 1
+            });
+        });
+    }
     return {
+        categories: categories,
         items: productList,
-        categories: categories
     };
 }
 
@@ -21,11 +44,20 @@ const mapProductItem = (product) => {
         title: product.title,
         picture: product.thumbnail,
         condition: product.condition,
+        category_id: product.category_id,
         freeShipping: product.shipping.free_shipping,
+        city_address: product.address ? product.address.city_name || '' : '',
         price: {
             currency: product.currency_id,
             amount: product.price
         }
+    }
+}
+
+const mapCategories = (category) => {
+    return {
+        id: category.id,
+        title: category.name
     }
 }
 
@@ -42,5 +74,6 @@ const appendSignature = (responseModel) => {
 module.exports = {
     handleSearchResponse,
     mapProductItem,
-    appendSignature
+    appendSignature,
+    mapCategories
 }
